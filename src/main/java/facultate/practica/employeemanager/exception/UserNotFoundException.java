bfe880839921c7f5766a22b72cf402c27dee5848
Radu Dragos <127134292+RaduDragosFlorian@users.noreply.github.com>
2023-07-23 15:29:08 +0300

    Add files via upload

---
+++ src/main/java/facultate/practica/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package facultate.practica.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
